d4d05ec4a8c01e0f7035603d2221a1f82f2ceff3
Aisheair <ankitkargupta98313@gmail.com>
2025-03-11 20:17:02 +0530

    iteams

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,22 +2,22 @@ import { useState } from "react";
 
 
 function ListGroup() {
-  let iteams = ["New York", "san Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "san Francisco", "Tokyo", "London", "Paris"];
 
   let [selectedIndex, setSelectedIndex] = useState(-1);
   
   return (
     <>
       <h1>List</h1>
-      {iteams.length === 0 && <p>No item found</p>}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {iteams.map((iteam, index) => (
+        {items.map((item, index) => (
           <li
             className={ selectedIndex === index ? 'list-group-item active' : 'list-group-iteam'}
-            key={iteam}
+            key={item}
             onClick={() => setSelectedIndex(index)}
           >
-            {iteam}
+            {item}
           </li>
         ))}
       </ul>
