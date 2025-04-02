e334c0c1821135c4093075ae081cec4161c7139e
Aisheair <ankitkargupta98313@gmail.com>
2025-04-03 00:12:48 +0530

    vanilla css

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+// import { useState } from "react";
+import ListGroup from "./components/ListGroup";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
 
 
 
 function App() {
-  const [alertVisible, setalertVisible] = useState(false);
+  // const [alertVisible, setalertVisible] = useState(false);
+  let items = ["New York", "san Francisco", "Tokyo", "London", "Paris"]
+  let log = (item: string) => console.log("clicked", item);
+  
 
   return (
     <div>
-    {alertVisible && <Alert onClose={() => setalertVisible(false)}>My Alert</Alert>}
-      <Button color="secondary" onClick={() => setalertVisible(true)}>My Button</Button>
+    {/* {alertVisible && <Alert onClose={() => setalertVisible(false)}>My Alert</Alert>}
+      <Button color="secondary" onClick={() => setalertVisible(true)}>My Button</Button> */}
+      <ListGroup items={items} heading="state" onSelectItem={log}/>
     </div>
   );
 }
