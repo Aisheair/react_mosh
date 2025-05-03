169bd794df9569fc91e7ecd0365b1ce6aa4453e4
Aisheair <ankitkargupta98313@gmail.com>
2025-05-03 23:31:55 +0530

    immer

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
-    {id: 1, title: 'Bug1', fixed: false},
-    {id: 2, title: 'Bug2', fixed: false},
+    { id: 1, title: "Bug1", fixed: false },
+    { id: 2, title: "Bug2", fixed: false },
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true}: bug))
-  }
+    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true}: bug))
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  };
 
   return (
-    
     <div>
-
-        <button onClick={handleClick}>Click Me</button>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "fixed" : "new"}
+        </p>
+      ))}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
