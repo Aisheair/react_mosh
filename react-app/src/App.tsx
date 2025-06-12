db7d5e81fbb62024ce7d74e65cbf6a2fd5a804ca
Aisheair <ankitkargupta98313@gmail.com>
2025-06-12 19:06:20 +0530

    updating array

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
 function App() {
-  const [tags, setTages] = useState(['happy', 'cheerful']);
+  const [tags, setTages] = useState(["happy", "cheerful"]);
 
   const handleClick = () => {
+        //Add
+    setTages([...tags, "exciting"])
 
+    // Remove
+    setTages(tags.filter(tag => tag ! == 'happy'))
+
+    // Update
+    setTages(tags.map(tag => tag === 'happy' ? 'happiness' : tag ))
   };
 
   return (
