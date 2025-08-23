deb481247f0f56b351f6a617ed4218b34d56d2f4
Aisheair <ankitkargupta98313@gmail.com>
2025-08-23 18:58:28 +0530

    how to show error

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,14 +8,18 @@ interface User {
 }
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('')
 
   useEffect(() => {
     axios
-      .get<User[]>("http://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<User[]>("http://jsonplaceholder.typicode.com/dusers")
+      .then((res) => setUsers(res.data))
+      .catch(err=> setError(err.message)
+      )
   },[]);
 
   return <>
+  {error && <p className="text-danger">{error}</p>}
   <ul>
     {users.map(user=> <li key={user.id}>{user.name}</li>)}
     </ul>
